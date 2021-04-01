48fe8f499b7ed351f964502f1f3221fb57b5d1fc
zaha <zahadhamov@gmail.com>
2021-04-01 22:52:06 +0300

    add finded labs :)

---
+++ notepad/post.rb
@@ -0,0 +1,41 @@
+class Post
+
+	def self.post_types
+		[Memo, Link, Task]
+	end
+
+	def self.create(type_index)
+		return post_types[type_index].new		
+	end
+
+	def initialize
+	@craeted_at = Time.now
+	@text = nil
+	end
+
+	def read_from_console
+		
+	end
+
+	def to_strings
+
+		
+	end
+
+	def save
+		file = File.new(file_path, "w:UTF-8")
+
+		for item in to_strings do 
+			file.puts(item)
+		end
+		file.close
+	end
+
+	def file_path
+		current_path = File.dirname(__FILE__)
+
+		file_name = @craeted_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")
+		return current_path + "/" + file_name
+		
+	end
+end
